11a84df71a7f3de71d8c68dc089674727603ea2e
Maximilian Mussotter <mussott@lfluids43.mpi-stuttgart.mpg.de>
2017-02-01 16:33:51 +0100

    initial commit

---
+++ create_pdf.sh
@@ -0,0 +1,8 @@
+#export TEXINPUTS='$(pwd)':$TEXINPUTS
+export TEXINPUTS="$(pwd)/tex_supps/theme":$TEXINPUTS
+export TEXINPUTS="$(pwd)/tex_supps/beamerposter":$TEXINPUTS
+pdflatex *.tex
+bibtex *.aux
+pdflatex *.tex
+pdflatex *.tex
+find * -maxdepth 0 ! -name *.pdf ! -name *.tex ! -name *.sh -type f -delete 
